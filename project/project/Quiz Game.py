6b840548ef086d652f2140b59dea960aeb973731
AnishRamagalla <98275330+AnishRamagalla@users.noreply.github.com>
2022-01-23 22:03:47 +0530

    Add files via upload

---
+++ project/project/Quiz Game.py
@@ -0,0 +1,193 @@
+# the json module to work with json files 
+import json
+import tkinter
+from tkinter import *
+import random
+
+# load questions and answer choices from json file instead of the file
+with open('./data.json', encoding="utf8") as f:
+    data = json.load(f)
+
+# convert the dictionary in lists of questions and answers_choice 
+questions = [v for v in data[0].values()]
+answers_choice = [v for v in data[1].values()]
+
+answers = [2,1,3,1,0,1,2,0,3,3] 
+
+user_answer = []
+
+indexes = []
+def gen():
+    global indexes
+    while(len(indexes) < 5):
+        x = random.randint(0,9)
+        if x in indexes:
+            continue
+        else:
+            indexes.append(x)
+
+
+def showresult(score):
+    lblQuestion.destroy()
+    r1.destroy()
+    r2.destroy()
+    r3.destroy()
+    r4.destroy()
+     
+    global label,label1,label11,btn
+    label=Label(root,text="Your Score is",font="Verdana 24 bold",bg="#ffffff",fg="black",justify="center")
+    label.place(x=500,y=250)                
+    label1=Label(root,text=score,font="Verdana 24 bold",bg="#ffffff",fg="black",justify="center")
+    label1.place(x=750,y=250)
+
+    if score!=5:
+        label11 = Label(root,text="/5",font="Verdana 24 bold",bg="#ffffff",fg="black",justify="center")
+        label11.place(x=775,y=250)
+
+    else :
+        label11=Label(root,text="/5",font="Verdana 24 bold",bg="#ffffff",fg="black",justify="center")
+        label11.place(x=800,y=250)
+
+    btn1 = Button(text="Quit",font="bold 18",bg="#FF0000",fg="black",command=root.destroy)
+    btn1.place(x=675,y=400)
+                      
+def calc():
+    global indexes,user_answer,answers
+    x = 0
+    score = 0
+    for i in indexes:
+        if user_answer[x] == answers[i]:
+            score = score + 1
+        x += 1
+    print(score)
+    showresult(score)
+        
+ques = 1
+def selected():
+    global radiovar,user_answer
+    global lblQuestion,r1,r2,r3,r4
+    global ques
+    x = radiovar.get()
+    user_answer.append(x)
+    radiovar.set(-1)
+    if ques < 5:
+        lblQuestion.config(text= questions[indexes[ques]])
+        r1['text'] = answers_choice[indexes[ques]][0]
+        r2['text'] = answers_choice[indexes[ques]][1]
+        r3['text'] = answers_choice[indexes[ques]][2]
+        r4['text'] = answers_choice[indexes[ques]][3]
+        ques += 1
+    else:
+        # print(indexes)
+        # print(user_answer)
+        # these two lines were just developement code
+        # we don't need them
+        calc()
+
+def startquiz():
+    global lblQuestion,r1,r2,r3,r4
+    lblQuestion = Label(
+        root,
+        text = questions[indexes[0]],
+        font = ("Consolas", 16),
+        width = 500,
+        justify = "center",
+        wraplength = 400,
+        background = "#ffffff",
+    )
+    lblQuestion.pack(pady=(100,30))
+
+    global radiovar
+    radiovar = IntVar()
+    radiovar.set(-1)
+
+    r1 = Radiobutton(
+        root,
+        text = answers_choice[indexes[0]][0],
+        font = ("Times", 12),
+        value = 0,
+        variable = radiovar,
+        command = selected,
+        background = "#ffffff",
+    )
+    r1.pack(pady=5)
+
+    r2 = Radiobutton(
+        root,
+        text = answers_choice[indexes[0]][1],
+        font = ("Times", 12),
+        value = 1,
+        variable = radiovar,
+        command = selected,
+        background = "#ffffff",
+    )
+    r2.pack(pady=5)
+
+    r3 = Radiobutton(
+        root,
+        text = answers_choice[indexes[0]][2],
+        font = ("Times", 12),
+        value = 2,
+        variable = radiovar,
+        command = selected,
+        background = "#ffffff",
+    )
+    r3.pack(pady=5)
+
+    r4 = Radiobutton(
+        root,
+        text = answers_choice[indexes[0]][3],
+        font = ("Times", 12),
+        value = 3,
+        variable = radiovar,
+        command = selected,
+        background = "#ffffff",
+    )
+    r4.pack(pady=5)
+
+
+def startIspressed():
+    labelimage.destroy()
+    labeltext.destroy()
+    btnStart.destroy()
+    gen()
+    startquiz()
+
+root = tkinter.Tk()
+root.title("Quiz Game")
+root.geometry("700x600")
+root.config(background="#ffffff")
+#root.resizable(0,0)
+
+img1 = PhotoImage(file="Quiz Game.png")
+
+labelimage = Label(
+    root,
+    image = img1,
+    background = "#ffffff",
+)
+labelimage.pack(pady=(40,0))
+
+labeltext = Label(
+    root,
+    text = "Quiz App",
+    font = ("Comic sans MS",24,"bold"),
+    background = "#ffffff",
+)
+labeltext.pack(pady=(0,10))
+
+img2 = PhotoImage(file="Frame.png")
+
+btnStart = Button(
+    root,
+    image = img2,
+    relief = FLAT,
+    border = 0,
+    command = startIspressed,
+)
+btnStart.pack()
+#win=tkinter.Tk()
+#win.title("Let's play Quiz")
+#win.configure(bg="#FF0000")
+ #win.geometry("1350x700+0+0")
+root.mainloop()
